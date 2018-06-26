aa95a48363492c190db6d759b5252e8410c2ad5f
Jason Bobich <jasonbobich@gmail.com>
2018-06-26 15:11:31 -0600

    Add <Home> view component with state and setup for marvel API search

---
+++ client/src/components/home/index.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { marvel, cleanCharacters } from '../../utils/characters';
+import SearchField from './SearchField';
+import CharacterList from '../shared/CharacterList';
+
+/**
+ * Home View
+ */
+class Home extends Component {
+    /**
+     * Class constructor.
+     */
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchQuery: '',
+            searchError: '',
+            characters: [] // Found characters.
+        };
+    }
+
+    /**
+     * Handle searching.
+     */
+    handleSearch = event => {
+        const { value } = event.target;
+
+        if (!value) {
+            this.setState({
+                searchError: '',
+                characters: []
+            });
+        }
+
+        this.setState({ searchQuery: value });
+
+        marvel.characters
+            .findNameStartsWith(value)
+            .then(response => {
+                this.setState({
+                    searchError: '',
+                    characters: cleanCharacters(response.data)
+                });
+            })
+            .catch(error => {
+                this.setState({
+                    searchError: 'An error occurred.',
+                    characters: []
+                });
+            });
+    };
+
+    /**
+     * Render component.
+     *
+     * @return {Component}
+     */
+    render() {
+        const { searchQuery, characters } = this.state;
+
+        return (
+            <div className="view-home">
+                <div className="wrap">
+                    <SearchField
+                        value={searchQuery}
+                        onChange={this.handleSearch}
+                    />
+                    <CharacterList characters={characters} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
